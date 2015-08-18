ea96f09ac423de011b98e9c0787beb7eab226462
Nickolas Nikolic <subclass@gmail.com>
2015-08-18 14:58:23 -0500

    first hellos

---
+++ js/first.js
@@ -0,0 +1,18 @@
+/**
+ * Created by nickolasnikolic on 8/18/15.
+ */
+$(document).ready(function(){
+    var stage = createjs.Stage('stage');
+
+    var circle = new createjs.Shape();
+    circle.graphics
+        .beginFill('red')
+        .drawCircle(0,0,50);
+
+    circle.x = 100;
+    circle.y = 100;
+
+    stage.addChild(circle);
+
+    stage.update();
+});
